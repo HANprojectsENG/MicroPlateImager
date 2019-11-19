c59402ec9c98e886545f38860c17c4698b0ae36c
GertvL <G.vanlagen@student.han.nl>
2019-11-19 13:59:45 +0100

    cleaner seperation of serial and gui class

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ from PyQt5.QtSerialPort import QSerialPort
 from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QDialog
 
 class Gcode_controller(QDialog):
+
+    PrintHAT_serial = serial_comm.ser_comm()
+    
     def __init__(self, parent=None):
         super().__init__()
         
         layout = QVBoxLayout()
-
+        
+        # button CONNECT
         self.b_connect = QPushButton("CONNECT")
         self.b_connect.setCheckable(True)
         self.b_connect.setChecked(True)
@@ -18,6 +22,42 @@ class Gcode_controller(QDialog):
         self.b_connect.clicked.connect(self.btnstate)
         layout.addWidget(self.b_connect)
 
+        # button HOME X
+        self.b_home_x = QPushButton("HOME X")
+        self.b_home_x.setCheckable(True)
+        self.b_home_x.setChecked(False)
+        self.b_home_x.clicked.connect(self.homeX)
+        layout.addWidget(self.b_home_x)
+
+        # button GET_POSITION
+        self.b_get_pos = QPushButton("GET_POSITION")
+        self.b_get_pos.setCheckable(False)
+        self.b_get_pos.setChecked(False)
+        self.b_get_pos.clicked.connect(self.btnstate)
+        layout.addWidget(self.b_get_pos)
+
+        # button FIRMWARE_RESET
+        self.b_firm_rest = QPushButton("FIRMWARE_RESTART")
+        self.b_firm_rest.setCheckable(False)
+        self.b_firm_rest.setChecked(False)
+        self.b_firm_rest.clicked.connect(self.btnstate)
+        layout.addWidget(self.b_firm_rest)
+
+        # button G0 X0
+        self.b_x_zero = QPushButton("G0 X0")
+        self.b_x_zero.setCheckable(False)
+        self.b_x_zero.setChecked(False)
+        self.b_x_zero.clicked.connect(self.btnstate)
+        layout.addWidget(self.b_x_zero)
+
+        # button G0 X10
+        self.b_x_ten = QPushButton("G0 X10")
+        self.b_x_ten.setCheckable(False)
+        self.b_x_ten.setChecked(False)
+        self.b_x_ten.clicked.connect(self.btnstate)
+        layout.addWidget(self.b_x_ten)
+        
+        # button READ FOR 10S
         self.b_read = QPushButton("READ FOR 10S")
         self.b_read.setCheckable(True)
         self.b_read.setChecked(True)
@@ -30,35 +70,63 @@ class Gcode_controller(QDialog):
 
     def btnstate(self):
         # button CONNECT / DISCONNECT
-        PrintHAT_serial = serial_comm.ser_comm("/dev/ttyAMA0", None)
-        
         if self.b_connect.isChecked():
             self.b_connect.setText("DISCONNECT")
-            if not PrintHAT_serial.isOpen:
-                PrintHAT_serial = serial_comm.ser_comm("/dev/ttyAMA0", None)           
-            PrintHAT_serial.writeHelloWorld()
+            self.PrintHAT_serial.connect("/tmp/printer") # = serial_comm.ser_comm("/tmp/printer", None)
         else:
             self.b_connect.setText("CONNECT")
-            if PrintHAT_serial.isOpen:
-                PrintHAT_serial.disconnect()
+            self.PrintHAT_serial.disconnect()
 
         # button START READING / STOP READING
         if self.b_read.isChecked():
             if self.b_connect.isChecked():
-                self.b_read.setText("STOP READING")
                 ready_to_read = PrintHAT_serial.dataAvailable()
                 if ready_to_read == False:
                     print("no data")
                 else:
                     print("Data captured:")
                     PrintHAT_serial.readData()
-                    self.b_read.setChecked(False)
-                    self.b_read.setText("READ FOR 10S")
             else:
                 print("You try to read data while port is not connected")
-        else:    
-            self.b_read.setText("READ FOR 10S")                            
-                    
+
+        # button FIRMWARE_RESTART
+        if self.b_firm_rest.isChecked():
+            if self.b_connect.isChecked():
+                PrintHAT_serial.writeFirmwareRestart()
+            else:
+                print("Please connect first with your serial port")
+
+        # button GET_POSITION
+        if self.b_get_pos.isChecked():
+            if self.b_connect.isChecked():
+                PrintHAT_serial.writeGetPosition()
+            else:
+                print("Pleas connect first with your serial port")
+
+                
+        # button G0 X0
+        if self.b_x_zero.isChecked():
+            if self.b_connect.isChecked():
+                PrintHAT_serial.writeXZero()
+            else:
+                print("Pleas connect first with your serial port")
+
+
+        # button G0 X10
+        if self.b_x_ten.isChecked():
+            if self.b_connect.isChecked():
+                PrintHAT_serial.writeXTen()
+            else:
+                print("Pleas connect first with your serial port")
+
+    def homeX(self):
+        # button HOME X
+        if self.b_home_x.isChecked():
+            if self.b_connect.isChecked():
+                self.PrintHAT_serial.writeHomeX()
+            else:
+                print("Please connect first with your serial port")
+
 ####################################################
 ################# MAIN APPLICATION #################
 ####################################################
--- serial_comm.py
+++ serial_comm.py
@@ -3,10 +3,11 @@ from PyQt5.QtSerialPort import QSerialPort
 from PyQt5.QtWidgets import *
 
 class ser_comm:
-    def __init__(self, port, parent=None):
+    def __init__(self, parent=None):
         super().__init__()
         self.isOpen = False
-        
+
+    def connect(self, port):
         # OPEN THE SERIAL PORT
         self.serial = QSerialPort(port)
         self.serial.setPortName(port)
@@ -14,22 +15,51 @@ class ser_comm:
         if self.serial.open(QIODevice.ReadWrite):
             self.isOpen = True
             print("Opened serial communication on port {}".format(port))
-            self.serial.setBaudRate(115200)
+            self.serial.setBaudRate(250000)
             
         else:
             raise IOError("Cannot connect to device on port {}".format(port))
             self.isOpen = False
-
-    def writeHelloWorld(self):
-        print("\nDEBUG: in function ser_comm::writeHelloWorld()")
-        homing = QByteArray()
-        homing.append("Hello World\r\n")
-        self.serial.write(homing)
         return
+    
+    def writeFirmwareRestart(self):
+        print("\nDEBUG: in function ser_comm::writeFirmwareRestart()")
+        firm_rest = QByteArray()
+        firm_rest.append("FIRMWARE_RESTART\r\n")
+        self.serial.write(firm_rest)
+        return
+
+    def writeHomeX(self):
+        print("\nDEBUG: in function ser_com::writeHomeX()")
+        HomeX = QByteArray()
+        HomeX.append("G28 X0")
+        self.serial.write(HomeX)
+        return
+
+    def writeGetPosition(self):
+        print("\nDEBUG: in function ser_com::writeGetPosition()")
+        position = QByteArray()
+        position.append("M114")
+        self.serial.write(position)
+        return
+
+    def writeXZero(self):
+        print("\nDEBUG: in function ser_com::writeXZero()")
+        position = QByteArray()
+        position.append("G0 X0")
+        self.serial.write(position)
+        return    
+
+    def writeXten(self):
+        print("\nDEBUG: in function ser_com::writeXTen()")
+        position = QByteArray()
+        position.append("G0 X10")
+        self.serial.write(position)
+        return  
     
     def dataAvailable(self):
         print("\nDEBUG: in function ser_comm::dataAvailable()")
-        readLine = self.serial.waitForReadyRead(10000)
+        readLine = self.serial.waitForReadyRead(2000)
         return readLine
 
     def readData(self):
